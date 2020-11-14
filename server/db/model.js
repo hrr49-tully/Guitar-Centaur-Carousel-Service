2cbcd06103984658f2b90e8f84dabaf575f15b4c
dwwr <dwwarnerreyes@gmail.com>
2020-11-13 18:28:55 -0800

    added api endpoints

--- server/db/model.js
+++ server/db/model.js
@@ -1,1 +1,23 @@
 //methods for interacting with DB and formatting data will go here
+var db = require('../db');
+
+
+module.exports = {
+  getAllItems: () => {
+    return new Promise ((resolve, reject) => {
+      let query = `SELECT * FROM items`;
+      db.query(query, (err, results) => {
+        err ? reject(err) : resolve(results);
+      });
+    });
+
+  },
+  getAllPhotos: () => {
+    return new Promise ((resolve, reject) => {
+      let query = `SELECT * FROM items`;
+      db.query(query, (err, results) => {
+        err ? reject(err) : resolve(results);
+      });
+    });
+  }
+};
--- server/index.js
+++ server/index.js
@@ -1,13 +1,20 @@
 const express = require('express');
 const app = express();
 const port = 3000;
+const db = require('./db/index.js');
+const model = require('./db/model.js');
+const bodyParser = require('body-parser');
+
+app.use(bodyParser.json());
 
 // app.use(express.static('../public'));
 app.use(express.static(__dirname + '/../public'));
 
-// app.get('/', (req, res) => {
-//   res.sendFile('../public/index.html')
-// });
+app.get('/items', async (req, res) => {
+  let items = await model.getAllItems()
+  console.log(items);
+  res.end(JSON.stringify(items));
+});
 
 app.listen(port, () => {
   console.log(`The Guitar Centaur rocks out to http://localhost:${port}`)
